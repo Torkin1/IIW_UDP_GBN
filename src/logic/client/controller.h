82e7af616d837cf70ce6815ce54fea92f752134a
Daniele La Prova <daniele.laprova2@gmail.com>
2021-10-03 13:40:49 +0200

    added to comments more detailed informations about error returning

--- src/logic/client/controller.h
+++ src/logic/client/controller.h
@@ -5,14 +5,14 @@
 
 /**
     Extracts command name from command line and retrieves the op code corresponding to extracted name if there is one, else returns an error
-    @return 0 on success, else -1
+    @return 0 on success, -1 if number of args is incorrect, or no command code is registered with given name
 */
 int parseCommandName(int argc, char *argv[], DmProtocol_command *toInvoke);
 
 /*
     calls routine corresponding to given command code, if there is one.
     Only implemented operations shall have a routine to be called.
-    @return 0 on success, else -1
+    @return 0 on success, -1 if no routine is registered to given command, or the command invoked failed
 */
 int doCommand(DmProtocol_command toInvoke, char *toInvokeArgs[]);
 
