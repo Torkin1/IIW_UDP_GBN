91a466c0a147e24976134d2e0c0d767ac900f1c9
Daniele La Prova <daniele.laprova2@gmail.com>
2021-10-04 19:25:55 +0200

    commit of client main

--- src/logic/client/main.c
+++ src/logic/client/main.c
@@ -1,81 +1,23 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include "../logger/logger.h"
+#include "client/controller.h"
+#include "logger/logger.h"
+#include "dm_protocol/dm_protocol.h"
 
+int main(int argc, char *argv){
 
-#define SIZE 1024 //un generico buffersieze
-
- int main(){
-
-  // Scrivo ip e porta, stessa tra server e client
-  char *ip = "127.0.0.1";
-  int port = 8080;
-
-// Definisco le variabili
-  int server_sockfd; //descrittore del socket
-  struct sockaddr_in server_addr;//struttura socket
-  FILE *fp;//descrittore del file
-  char *filename = "panda";//nome file che creo
-  // Creao la socket UDP, si usano le sock_dgram sempre IPv4, con IP (lo 0 finale)
-    server_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (server_sockfd < 0){
-      perror("[ERRORE] impossibile creare la socket");
-      exit(1);
-    }
-  //struttura della socket
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
-
-  // Leggento il ifle client.txt, demo da cancellare TODO
-  fp = fopen(filename, "r");
-  if (fp == NULL){
-      perror("[ERRORE] Impossibile leggere il file");
-      exit(1);
+  DmProtocol_command toInvoke;
+  char **toInvokeArgs;
+  
+  if (parseCommandName(argc, argv, &toInvoke) < 0){
+      logMsg(E, "main: failed to parse command name\n");
+      return EXIT_FAILURE;
   }
 
-    // Invio del file al server
-  send_file_data(fp, server_sockfd, server_addr);
+  toInvokeArgs = argv + 1;
+  if (doCommand(toInvoke, toInvokeArgs) < 0){
+    logMsg(E, "main: operation failed\n");
+    return EXIT_FAILURE;
+  }
 
-  //Dei print per chiarezza
-  printf("[SUCCESSO] transferimento dei dati completato.\n");
-  //TODO metterlo in un while e aspettare sempre comandi, tra cui chiusura
-  printf("[Chiusura] Disconnessioen dal server.\n");
-
-  close(server_sockfd);
-  return 0;
-}
-
-//Invio di un file, con fp cioè descrittore del file, socketfd è la scoket e la
-//sua struttura, del client
-void send_file_data(FILE *fp, int sockfd, struct sockaddr_in addr){
-  //variabili locali
-  int n;
-  char buffer[SIZE];
-
-  // Invio dati
-  while(fgets(buffer, SIZE, fp) != NULL){//leggo il file con un while
-      printf("[INVIO] Data: %s", buffer);//stampo cosa leggo
-      logMsg(I, "[INVIO] Data: %s\n", index);
-
-
-      //Invio i dati con sendto
-      n = sendto(sockfd, buffer, SIZE, 0, (struct sockaddr*)&addr, sizeof(addr));
-      if (n == -1){
-        perror("[ERRORE] impossibile inviare dato al server.");
-        exit(1);
-      }
-      bzero(buffer, SIZE);//azzero il buffer
-
-    }
-
-    // Invio dell'"END" per poter definire la fine del file, fare così o meglio
-    //come se fosse un comando la prossima volta, preso da input del client
-    strcpy(buffer, "END");
-    sendto(sockfd, buffer, SIZE, 0, (struct sockaddr*)&addr, sizeof(addr));
-    fclose(fp);
-    return;
+  return EXIT_SUCCESS;
 }
